195551f544f0fab8a8676118af021402f222c0ec
Peter Scriven <pjpscriv@gmail.com>
2024-06-08 10:28:57 +1200

    Move key to secrets

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { KEY } from '../key';
 
 if (environment.production) {
   enableProdMode();
 }
+
+const scriptTag = document.createElement('script');
+scriptTag.src = `https://maps.googleapis.com/maps/api/js?key=${KEY}&libraries=places`;
+scriptTag.async = true;
+document.head.appendChild(scriptTag);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
